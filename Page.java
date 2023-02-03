915cbbd1d2c2f81ff4678ca44e8d8004c7e79a96
Nkorsah <Nkorsah21@gmail.com>
2023-02-03 18:52:17 -0500

    Update Page.java

--- Page.java
+++ Page.java
@@ -1,16 +1,27 @@
 package ClockDisplayJava;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Dimension;
 import javax.swing.BorderFactory;
 
 public class Page {
+        JFrame frame;
+        JPanel panel;
+        JLabel label; //scope so that all methods see this
 
     public Page() {
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
+        frame = new JFrame();
+        panel = new JPanel();
+        label = new JLabel("4:00 AM");
+
         panel.setBorder(BorderFactory.createEmptyBorder(300, 600, 300, 600));
+        panel.add(label);
+        label.setFont(new Font("Courier", Font.PLAIN, 72));
+        label.setPreferredSize(new Dimension(300,200));
         // panel.setLayout(new GridLayout(0,1));
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,7 +29,13 @@ public class Page {
         frame.pack();
         frame.setVisible(true);
     }
-    public static void main(String[] args){
+
+    public void update(){
+        label.setText("Haha lol");
+    }
+    public static void main(String[] args) throws InterruptedException{
         new Page();
+        Thread.sleep(6000);
+        update();
     }
 }
